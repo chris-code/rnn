33f9f8a6caf0d18bfea5f49e319cdf62d033ce1b
chris-code <christian.mielers@rub.de>
2017-10-03 16:23:33 +0200

    Split pg code in dataset building and training

--- src/pg_build_dataset.py
+++ src/pg_build_dataset.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python3
 
+import argparse
 import glob
+import gzip
 import html.parser
+import json
+import more_itertools
+import numpy as np
 import re
 import signal
 
-PG_ROOT = "/home/chrisbot/Projects/Geany-Workspace/RNN/data/pg_raw"
+
+def parse_args():
+	parser = argparse.ArgumentParser(description="Preprocess household power consumtion data set")
+	parser.add_argument("-f", "--test-fraction", type=float, default=0.1, help="Portion of the data to use for the test set")
+	parser.add_argument("-i", "--input-length", type=int, default=90, help="Input sequence length")
+	parser.add_argument("-s", "--step", type=int, default=30, help="Offset from one chunk to the next")
+	parser.add_argument("origin", help="The unprocessed data")
+	parser.add_argument("train_path", metavar="train-path", help="The unprocessed data")
+	parser.add_argument("test_path", metavar="test-path", help="The unprocessed data")
+	return parser.parse_args()
+
 
 class PG_HTML_parser(html.parser.HTMLParser):
 	def __init__(self):
@@ -28,29 +43,74 @@ class PG_HTML_parser(html.parser.HTMLParser):
 			self.is_p = False
 
 
-def simplify_content(content):
+def simplify_text(content):
 	content = " ".join(content.split())
-
-	content = re.sub(r"\[.*?\]", "", content)
-
-	content = re.sub(r"[^\w \.,:;!?|@#$%&()/_+\"-]", "", content, flags=re.ASCII)
-
+	content = re.sub(r"\[.*?\]", "", content) # Replace [...] (usually references like [1])
+	content = re.sub(r"[^\w \.,:;!?|@#$%&()/_+\"-]", "", content, flags=re.ASCII) # Remove all but the listet characters
 	content = content.lower()
 
 	return content
 
 
+def create_chunks(data, length, offset):
+	chunks = more_itertools.windowed(data, length, step=offset)
+	chunks = more_itertools.rstrip(chunks, lambda x: None in x) # Skip last chunk if it is incomplete
+
+	return list(chunks)
+
+
+def vectorize(chunks):
+	alphabet = sorted(set().union(*chunks))
+	char_to_idx = {c: i for i, c in enumerate(alphabet)}
+	idx_to_char = {i: c for i, c in enumerate(alphabet)}
+
+	data = np.zeros((len(chunks), len(chunks[0]), len(alphabet)), dtype=np.bool)
+	for idx_chunk, chunk in enumerate(chunks):
+		for idx_char, char in enumerate(chunk):
+			data[idx_chunk, idx_char, char_to_idx[char]] = 1
+
+	return data, idx_to_char
+
+
+def split_train_test(data, test_fraction):
+	split_idx = round(data.shape[0] * (1-test_fraction))
+	train = data[:split_idx]
+	test = data[split_idx:]
+
+	X_train, y_train, X_test, y_test = train[:,:-1,:], train[:,-1:,:], test[:,:-1,:], test[:,-1:,:]
+	return X_train, y_train, X_test, y_test
+
+
+def save_data(path, X, y, idx_to_char):
+	if not path.endswith(".npy.gz"):
+		raise ValueError("Output path must end in .npy.gz")
+
+	data = np.concatenate([X, y], axis=1)
+	with gzip.GzipFile(path, "w") as outfile:
+		np.save(outfile, data)
+
+	codepath = path.replace(".npy.gz", ".json")
+	with open(codepath, "w") as outfile:
+		json.dump(idx_to_char, outfile, sort_keys=True)
+
+
 if __name__ == "__main__":
 	signal.signal(signal.SIGPIPE, signal.SIG_DFL)
+	args = parse_args()
 
-	for inpath in sorted(glob.glob(PG_ROOT + "/*.html")):
+	parser = PG_HTML_parser()
+	for inpath in sorted(glob.glob(args.origin + "/*.html")):
 		with open(inpath) as infile:
 			html = infile.read()
-
-		parser = PG_HTML_parser()
 		parser.feed(html)
 
-		content = parser.content
-		content = simplify_content(content)
+	data = parser.content
+	data = simplify_text(data)
 
-		print(content, end=" ")
+	chunks = create_chunks(data, args.input_length+1, args.step)
+
+	data, idx_to_char = vectorize(chunks)
+	X_train, y_train, X_test, y_test = split_train_test(data, args.test_fraction)
+
+	save_data(args.train_path, X_train, y_train, idx_to_char)
+	save_data(args.test_path, X_test, y_test, idx_to_char)
--- src/pg_train.py
+++ src/pg_train.py
@@ -1,72 +1,55 @@
 #!/usr/bin/env python3
 
 import argparse
+import gzip
+import json
 import keras
 from keras.layers import Dense, Dropout
 from keras.layers.recurrent import LSTM
-import more_itertools
 import numpy as np
-import random
 import signal
-
-PG_PATH = "/home/chrisbot/Projects/Geany-Workspace/RNN/data/pg.txt"
-SENTENCE_LENGTH = 90
-PREDICT_LENGTH = 30
 
 
 def parse_args():
 	parser = argparse.ArgumentParser(description="Train recurrent network")
-	parser.add_argument("path", help="Text corpus")
-	parser.add_argument("-l", "--limit", type=int, help="Maximum number of bytes to load from dataset")
-	parser.add_argument("-e", "--epochs", type=int, default=1, help="Maximum number of bytes to load from dataset")
-	parser.add_argument("-b", "--batch-size", type=int, default=128, dest="batch_size", help="Maximum number of bytes to load from dataset")
+	parser.add_argument("-l", "--limit", type=int, help="Maximum number of data points to load from dataset")
+	parser.add_argument("-t", "--num-targets", type=int, dest="num_targets", default=1, help="Length of the target sequence")
+	parser.add_argument("-b", "--batch-size", type=int, default=128, dest="batch_size", help="How many points comprise a batch update")
+	parser.add_argument("-e", "--epochs", type=int, default=1, help="Number of epochs to train for")
+	parser.add_argument("path", help="Training data")
+	parser.add_argument("output_path", metavar="output-path", help="Where to store the model")
 	return parser.parse_args()
 
 
-def load_data(path, limit=None):
-	with open(path) as infile:
-		return infile.read(limit)
+def load_data(path, num_targets, limit=None):
+	with gzip.GzipFile(path, "r") as infile:
+		data = np.load(infile)
+	data = data[:limit]
+
+	X = data[:,:-num_targets,:]
+	y = data[:,-num_targets:,:]
+
+	codepath = path.replace(".npy.gz", ".json")
+	with open(codepath, "r") as infile:
+		idx_to_char = json.load(infile)
+
+	return X, y, idx_to_char
 
 
-def create_sentences(data, sentence_length):
-	sentence_length += 1 # The last one will be the target
-	chunks = more_itertools.windowed(data, sentence_length, step=sentence_length//3)
-	chunks = more_itertools.rstrip(chunks, lambda x: None in x) # Skip last chunk because it may be too short
-
-	sentences, next_chars = [], []
-	for c in chunks:
-		c = "".join(c)
-		sentences.append(c[:-1])
-		next_chars.append(c[-1])
-
-	return sentences, next_chars
-
-
-def vectorize(sentences, next_chars, alphabet):
-	char_to_idx = {c: i for i, c in enumerate(alphabet)}
-	idx_to_char = {i: c for i, c in enumerate(alphabet)}
-
-	X = np.zeros((len(sentences), len(sentences[0]), len(alphabet)), dtype=np.bool)
-	y = np.zeros((len(sentences), len(alphabet)), dtype=np.bool)
-
-	for idx_s, sentence in enumerate(sentences):
-		for idx_c, char in enumerate(sentence):
-			X[idx_s, idx_c, char_to_idx[char]] = 1
-		y[idx_s, char_to_idx[next_chars[idx_s]]] = 1
-
-	return X, y, char_to_idx, idx_to_char
-
-
-def build_model(sentence_length, alphabet_size):
+def build_model(sequence_length, alphabet_size):
 	optimizer = keras.optimizers.Adadelta(lr=0.1)
 
 	model = keras.models.Sequential()
-	model.add(LSTM(256, input_shape=(sentence_length, alphabet_size)))
+	model.add(LSTM(256, input_shape=(sequence_length, alphabet_size)))
 	model.add(Dropout(0.5))
 	model.add(Dense(alphabet_size, activation="softmax"))
 	model.compile(loss="categorical_crossentropy", optimizer=optimizer)
 
 	return model
+
+
+def save_model(path, model):
+	model.save(path)
 
 
 def predict(model, seed, alphabet_size, char_to_idx):
@@ -82,29 +65,25 @@ if __name__ == "__main__":
 	signal.signal(signal.SIGPIPE, signal.SIG_DFL)
 	args = parse_args()
 
-	data = load_data(args.path, args.limit)
-	alphabet = sorted(set(data))
-	sentences, next_chars = create_sentences(data, SENTENCE_LENGTH)
-	X, y, char_to_idx, idx_to_char = vectorize(sentences, next_chars, alphabet)
-
-	print("Corpus size:\t{0} chars".format(len(data)))
-	print("Alphabet size:\t{0} ({1})".format(len(alphabet), "".join(alphabet)))
-	print("Sentences:\t{0} (length {1})".format(len(sentences), SENTENCE_LENGTH))
+	X, y, idx_to_char = load_data(args.path, args.num_targets, args.limit)
+	y = np.squeeze(y)
 	print("X.shape:\t{0}".format(X.shape))
 	print("y.shape:\t{0}".format(y.shape))
 
-	model = build_model(SENTENCE_LENGTH, len(alphabet))
+	model = build_model(X.shape[1], X.shape[2])
 	model.summary()
 	model.fit(X, y, batch_size=args.batch_size, epochs=args.epochs)
 
-	seed_idx = random.randint(0, len(data)-SENTENCE_LENGTH-1)
-	seed = data[seed_idx:seed_idx+SENTENCE_LENGTH]
-	pred = seed
-	for i in range(PREDICT_LENGTH):
-		pred += predict(model, pred[-SENTENCE_LENGTH:], len(alphabet), char_to_idx)
+	save_model(args.output_path, model)
 
-	print("Seed:\t" + seed)
-	print("Pred:\t" + pred)
+	#seed_idx = random.randint(0, len(data)-SENTENCE_LENGTH-1)
+	#seed = data[seed_idx:seed_idx+SENTENCE_LENGTH]
+	#pred = seed
+	#for i in range(PREDICT_LENGTH):
+		#pred += predict(model, pred[-SENTENCE_LENGTH:], len(alphabet), char_to_idx)
+
+	#print("Seed:\t" + seed)
+	#print("Pred:\t" + pred)
 
 
 
